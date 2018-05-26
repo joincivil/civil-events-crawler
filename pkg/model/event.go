297eb80ccb97d4b160ff418db82a8877930a1179
Peter Ng <peter@joincivil.com>
2018-05-26 16:38:25 -0400

    Additional work on event and event tests

--- pkg/model/event.go
+++ pkg/model/event.go
@@ -12,7 +12,7 @@ type CivilEvent struct {
 	// Timestamp is the time this event was created.
 	Timestamp int
 
-	// The payload data of the event.
+	// Payload is the data from the raw event.
 	Payload *CivilEventPayload
 }
 
@@ -20,20 +20,15 @@ type CivilEvent struct {
 // from the data in the generated event type from the sol/abi code.
 type CivilEventPayload struct {
 
-	// Internal map for field to CivilPayloadValue
-	payload map[string]*CivilEventPayloadValue
-}
-
-// KeysValues returns the map of keys to values
-func (p *CivilEventPayload) KeysValues() map[string]*CivilEventPayloadValue {
-	return p.payload
+	// PayloadMap stores the field to CivilPayloadValues
+	PayloadMap *map[string]*CivilEventPayloadValue
 }
 
 // Keys returns all the field/key names in the payload
 func (p *CivilEventPayload) Keys() []string {
-	keys := make([]string, len(p.payload))
+	keys := make([]string, len(*p.PayloadMap))
 	index := 0
-	for k := range p.payload {
+	for k := range *p.PayloadMap {
 		keys[index] = k
 		index++
 	}
@@ -42,9 +37,9 @@ func (p *CivilEventPayload) Keys() []string {
 
 // Values returns all the values in the payload
 func (p *CivilEventPayload) Values() []*CivilEventPayloadValue {
-	values := make([]*CivilEventPayloadValue, len(p.payload))
+	values := make([]*CivilEventPayloadValue, len(*p.PayloadMap))
 	index := 0
-	for _, v := range p.payload {
+	for _, v := range *p.PayloadMap {
 		values[index] = v
 		index++
 	}
@@ -53,40 +48,35 @@ func (p *CivilEventPayload) Values() []*CivilEventPayloadValue {
 
 // Value returns the value of the key.
 func (p *CivilEventPayload) Value(key string) (val *CivilEventPayloadValue, exists bool) {
-	val, exists = p.payload[key]
+	val, exists = (*p.PayloadMap)[key]
 	return val, exists
 }
 
 // CivilEventPayloadValue represents a value for a payload key.
 type CivilEventPayloadValue struct {
-	value interface{}
-}
-
-// Value returns the raw payload value.
-func (v *CivilEventPayloadValue) Value() interface{} {
-	return v.value
+	Value interface{}
 }
 
 // ToBool attempts to return the value as a bool
 func (v *CivilEventPayloadValue) ToBool() (val bool, ok bool) {
-	val, ok = v.value.(bool)
+	val, ok = v.Value.(bool)
 	return val, ok
 }
 
 // ToInt attempts to return the value as an int
 func (v *CivilEventPayloadValue) ToInt() (val int, ok bool) {
-	val, ok = v.value.(int)
+	val, ok = v.Value.(int)
 	return val, ok
 }
 
 // ToFloat64 attempts to return the value as an float64
 func (v *CivilEventPayloadValue) ToFloat64() (val float64, ok bool) {
-	val, ok = v.value.(float64)
+	val, ok = v.Value.(float64)
 	return val, ok
 }
 
 // ToString attempts to return the value as an string
 func (v *CivilEventPayloadValue) ToString() (val string, ok bool) {
-	val, ok = v.value.(string)
+	val, ok = v.Value.(string)
 	return val, ok
 }
