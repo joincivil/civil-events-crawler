3fcf18203315a2ac758344bb475d8244a454ae02
Peter Ng <peter@joincivil.com>
2018-10-23 11:08:41 -0400

    Added function to normalize hex string for eth addresses

--- pkg/utils/eth.go
+++ pkg/utils/eth.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
@@ -45,4 +46,12 @@ func (b *BlockHeaderCache) AddHeader(num uint64, header *types.Header) {
 		addedTs: CurrentEpochSecsInInt64(),
 		header:  header,
 	}
+}
+
+// NormalizeEthAddress takes a string address to normalize the
+// case of the ethereum address when it is a string.
+// Runs through common.Address.Hex().
+func NormalizeEthAddress(addr string) string {
+	address := common.HexToAddress(addr)
+	return address.Hex()
 }
