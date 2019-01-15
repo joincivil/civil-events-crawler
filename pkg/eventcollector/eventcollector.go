81e178b55203753bea0b1f8a19b4ba69efe10900
inna shteinbuk <ibs26@cornell.edu>
2019-01-15 18:41:27 -0500

    fixing error after merge with master

--- pkg/eventcollector/eventcollector.go
+++ pkg/eventcollector/eventcollector.go
@@ -145,10 +145,8 @@ func (c *EventCollector) handleEvent(payload interface{}) interface{} {
 	if c.crawlerPubSub != nil {
 		err = c.crawlerPubSub.PublishWatchedEventMessage(event)
 		if err != nil {
-			log.Errorf("Error sending message to pubsub: %v", err)
+			return fmt.Errorf("Error sending message to pubsub: %v", err)
 		}
-		errors <- err
-		return nil
 	}
 
 	// Update last block in persistence in case of error
