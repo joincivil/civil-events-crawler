f78175e7c7fdb3483ae7af8f197484b7b4de97db
inna shteinbuk <ibs26@cornell.edu>
2019-02-25 13:06:48 -0500

    adding model for table_version

---
+++ pkg/persistence/postgres/table_version.go
@@ -0,0 +1,32 @@
+package postgres // import "github.com/joincivil/civil-events-crawler/pkg/persistence/postgres"
+
+import (
+	"fmt"
+	// "github.com/ethereum/go-ethereum/common"
+)
+
+const (
+	versionTableName = "version"
+)
+
+// CreateVersionTableQuery returns the query to create the version table
+func CreateVersionTableQuery() string {
+	return CreateEventTableQueryString(versionTableName)
+}
+
+// CreateVersionTableQueryString returns the query to create this table
+func CreateVersionTableQueryString(tableName string) string {
+	queryString := fmt.Sprintf(`
+        CREATE TABLE IF NOT EXISTS %s(
+            version TEXT,
+            service_name TEXT
+        );
+    `, tableName)
+	return queryString
+}
+
+// VersionData is the model for the version table in DB
+type VersionData struct {
+	Version     string `db:"version"`
+	ServiceName string `db:"service_name"`
+}
