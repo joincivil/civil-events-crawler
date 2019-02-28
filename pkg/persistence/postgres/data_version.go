901fddeab755a1d94ef7463e73c76b073c14768b
inna shteinbuk <ibs26@cornell.edu>
2019-02-28 14:30:20 -0500

    Adding lastUpdatedTs and renaming tablename

--- pkg/persistence/postgres/data_version.go
+++ pkg/persistence/postgres/data_version.go
@@ -2,31 +2,28 @@ package postgres // import "github.com/joincivil/civil-events-crawler/pkg/persis
 
 import (
 	"fmt"
-	// "github.com/ethereum/go-ethereum/common"
 )
 
 const (
-	versionTableName = "version"
+	// VersionTableName is the table name for this model
+	VersionTableName = "data_version"
 )
 
-// CreateVersionTableQuery returns the query to create the version table
-func CreateVersionTableQuery() string {
-	return CreateVersionTableQueryString(versionTableName)
-}
-
-// CreateVersionTableQueryString returns the query to create this table
-func CreateVersionTableQueryString(tableName string) string {
+// CreateVersionTableQuery returns the query to create this table
+func CreateVersionTableQuery(tableName string) string {
 	queryString := fmt.Sprintf(`
         CREATE TABLE IF NOT EXISTS %s(
             version TEXT,
-            service_name TEXT
+            service_name TEXT,
+            last_updated_timestamp INT
         );
-    `, versionTableName)
+    `, tableName)
 	return queryString
 }
 
-// VersionData is the model for the version table in DB
-type VersionData struct {
-	Version     string `db:"version"`
-	ServiceName string `db:"service_name"`
+// Version is the model for the version table in DB
+type Version struct {
+	Version           *string `db:"version"`
+	ServiceName       string  `db:"service_name"`
+	LastUpdatedDateTs int64   `db:"last_updated_timestamp"`
 }
