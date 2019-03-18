3ea239ddcadbe191b3bf464e6a843e40a4801e86
Peter Ng <peter@joincivil.com>
2019-03-18 19:48:49 -0400

    Using our own subscription struct instead of go-eth's

---
+++ pkg/utils/net_test.go
@@ -0,0 +1,52 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/joincivil/civil-events-crawler/pkg/utils"
+)
+
+func TestIsWebsocketURL(t *testing.T) {
+	if utils.IsWebsocketURL("http://mydomain.com") {
+		t.Errorf("Should have returned false for http url")
+	}
+	if utils.IsWebsocketURL("https://mydomain.com") {
+		t.Errorf("Should have returned false for https url")
+	}
+	if !utils.IsWebsocketURL("ws://mydomain.com/ws") {
+		t.Errorf("Should have returned true for ws url")
+	}
+	if !utils.IsWebsocketURL("wss://mydomain.com/ws") {
+		t.Errorf("Should have returned true for wss url")
+	}
+}
+
+func TestSetupHTTPEthClient(t *testing.T) {
+	client, err := utils.SetupHTTPEthClient("https://rinkeby.infura.io/")
+	if err != nil {
+		t.Errorf("Should not have gotten error: err: %v", err)
+	}
+	if client == nil {
+		t.Errorf("Should not have nil client")
+	}
+
+	_, err = utils.SetupHTTPEthClient("wss://rinkeby.infura.io/ws")
+	if err == nil {
+		t.Errorf("Should have gotten error")
+	}
+}
+
+func TestSetupWsEthClient(t *testing.T) {
+	client, err := utils.SetupWebsocketEthClient("wss://rinkeby.infura.io/ws", nil, 0)
+	if err != nil {
+		t.Errorf("Should not have gotten error: err: %v", err)
+	}
+	if client == nil {
+		t.Errorf("Should not have nil client")
+	}
+
+	_, err = utils.SetupWebsocketEthClient("https://rinkeby.infura.io/ws", nil, 0)
+	if err == nil {
+		t.Errorf("Should have gotten error")
+	}
+}
