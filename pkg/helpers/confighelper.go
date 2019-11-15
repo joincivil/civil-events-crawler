903a36715546a010416b1128645ae946b3958f21
Peter Ng <peter@joincivil.com>
2019-11-15 15:54:11 -0500

    feat: add helper func persister from existing db
    add another func to help return an EventDataPersister from an existing
    sqlx.DB object

--- pkg/helpers/confighelper.go
+++ pkg/helpers/confighelper.go
@@ -3,11 +3,11 @@ package helpers
 
 import (
 	log "github.com/golang/glog"
+	"github.com/jmoiron/sqlx"
 
 	"github.com/joincivil/civil-events-crawler/pkg/model"
 	"github.com/joincivil/civil-events-crawler/pkg/persistence"
 	"github.com/joincivil/civil-events-crawler/pkg/persistence/postgres"
-	"google.golang.org/appengine/log"
 
 	cconfig "github.com/joincivil/go-common/pkg/config"
 )
@@ -16,45 +16,73 @@ import (
 // the given configuration
 func EventPersister(config cconfig.PersisterConfig) (model.EventDataPersister, error) {
 	if config.PersistType() == cconfig.PersisterTypePostgresql {
-		persister, err := persistence.NewPostgresPersister(
-			config.Address(),
-			config.Port(),
-			config.User(),
-			config.Password(),
-			config.Dbname(),
-			config.PoolMaxConns(),
-			config.PoolMaxIdleConns(),
-			config.PoolConnLifetimeSecs(),
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		// Pass nil to crawler persistence so it uses the latest version
-		err = persister.CreateVersionTable(nil)
-		if err != nil {
-			return nil, err
-		}
-		// Create event table
-		err = persister.CreateEventTable()
-		if err != nil {
-			return nil, err
-		}
-		// Attempts to create all the necessary indices on the tables
-		err = persister.CreateIndices()
-		if err != nil {
-			return nil, err
-		}
-
-		// Populate persistence with latest block data from events table
-		err = persister.PopulateBlockDataFromDB(postgres.EventTableBaseName)
-		if err != nil {
-			log.Errorf("Error populating event last occurrence block data: err: %v", err)
-		}
-
-		return persister, nil
+		return postgresPersister(config)
 	}
 
 	nullPersister := &persistence.NullPersister{}
 	return nullPersister, nil
+}
+
+// EventPersisterFromSqlx is a helper function to return the correct event persister from
+// a given sqlx.DB
+func EventPersisterFromSqlx(db *sqlx.DB) (model.EventDataPersister, error) {
+	persister, err := persistence.NewPostgresPersisterFromSqlx(db)
+	if err != nil {
+		return nil, err
+	}
+
+	err = initTablesAndData(persister)
+	if err != nil {
+		return nil, err
+	}
+
+	return persister, nil
+}
+
+func postgresPersister(config cconfig.PersisterConfig) (*persistence.PostgresPersister, error) {
+	persister, err := persistence.NewPostgresPersister(
+		config.Address(),
+		config.Port(),
+		config.User(),
+		config.Password(),
+		config.Dbname(),
+		config.PoolMaxConns(),
+		config.PoolMaxIdleConns(),
+		config.PoolConnLifetimeSecs(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	err = initTablesAndData(persister)
+	if err != nil {
+		return nil, err
+	}
+
+	return persister, nil
+}
+
+func initTablesAndData(persister *persistence.PostgresPersister) error {
+	// Pass nil to crawler persistence so it uses the latest version
+	err := persister.CreateVersionTable(nil)
+	if err != nil {
+		return err
+	}
+	// Create event table
+	err = persister.CreateEventTable()
+	if err != nil {
+		return err
+	}
+	// Attempts to create all the necessary indices on the tables
+	err = persister.CreateIndices()
+	if err != nil {
+		return err
+	}
+	// Populate persistence with latest block data from events table
+	err = persister.PopulateBlockDataFromDB(postgres.EventTableBaseName)
+	if err != nil {
+		log.Errorf("Error populating event last occurrence block data: err: %v", err)
+	}
+
+	return nil
 }
